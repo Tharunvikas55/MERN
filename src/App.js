b3455c8a7df2809319518fe7e1086cf6d6094dcb
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-03-05 12:51:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Cart from './Pages/Cart'
+import LoginSignup from './Pages/LoginSignup'
+
+function App() {
+  return (
+    <div >
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Shop/>}/>
+      <Route path="/product" element={<Product/>} />
+          <Route path=":productId" element={<Product/>}/>
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path='/login'  element={<LoginSignup/>} />
+    </Routes>
+    
+    </BrowserRouter>
+    
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo1.png'
+import cart from '../Assets/shoppingCart.png'
+import { set } from 'mongoose'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  const [menu,setMenu]=useState("products");
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" width="40px" height="40px"/>
+        <p>shopper</p>
+      </div>
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("products")}}> <Link to='/product' style={{textDecoration:'none'}} >Products</Link>  {menu==="products"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("about")}} >  <Link to='/about' style={{textDecoration:'none'}} >About</Link> {menu==="about"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("contact")}} >  <Link to='/contact' style={{textDecoration:'none'}} >Contact</Link> {menu==="contact"?<hr/>:<></>}</li>  
+      </ul>
+      <div className="nav-login-cart">
+        <Link to='/login'><button>login</button></Link>
+        <Link to='/cart'><img src={cart} alt="" width="30px" height="30px" /></Link>
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
